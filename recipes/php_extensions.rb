13ef6065ef7fa839e26dff94ec5308ca8afd0f75
John Levermore <john.levermore@zhibek.com>
2015-06-29 13:14:13 +0200

    Add (and enable) php5-mcrypt extension (required by Magento).

--- recipes/php_extensions.rb
+++ recipes/php_extensions.rb
@@ -2,6 +2,14 @@ package "php5-curl" do
   action :install
 end
 
+package "php5-mcrypt" do
+  action :install
+end
+
+execute "/usr/sbin/php5enmod mcrypt" do
+   only_if { platform?('ubuntu') && node['platform_version'].to_f >= 12.04 && ::File.exists?('/usr/sbin/php5enmod') }
+end
+
 package "php5-redis" do
   action :install
 end
