77d37544c5d2c7bd265b232e1bd333b95c69ec0f
John Levermore <john.levermore@zhibek.com>
2015-08-11 15:14:54 +0200

    Add php5-gd PHP extension for Magento image uploading / display.

--- recipes/php_extensions.rb
+++ recipes/php_extensions.rb
@@ -6,6 +6,7 @@ package "php5-mcrypt" do
   action :install
 end
 
+# Force mcrypt PHP extension to be enabled in Ubuntu (our environment)
 execute "/usr/sbin/php5enmod mcrypt" do
    only_if { platform?('ubuntu') && node['platform_version'].to_f >= 12.04 && ::File.exists?('/usr/sbin/php5enmod') }
 end
@@ -15,5 +16,9 @@ package "php5-redis" do
 end
 
 package "php5-mysql" do
+  action :install
+end
+
+package "php5-gd" do
   action :install
 end
